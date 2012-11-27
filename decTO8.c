dc62409c33fcf826fd610deb8be8f3688b858500
Tomasz Wilk <twilk@p29-7.labpk.inf.ug.edu.pl>
2012-11-27 17:03:36 +0100

    dodalem decTO8.c

---
+++ decTO8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+main()
+{
+int r,licz,sum;
+long oktagonalny,decymalny,temp;
+
+ licz=0;
+ sum=0;
+ decymalny=0;
+printf("Podaj liczbe w systemie dziesiatkowym do konwersji na system osemkowy: ");
+scanf("%ld",&decymalny);
+temp=decymalny;
+while(temp>0)
+{
+r=temp%8;
+sum=sum*10+r;
+temp=temp/8;
+licz++;
+}
+oktagonalny=0;
+while(licz>0)
+{
+r=sum%10;
+oktagonalny=oktagonalny*10+r;
+sum=sum/10;
+licz--;
+}
+printf("Osemkowy odpowiednik liczby dziesiatkowej %ld to %ld\n\n",decymalny,oktagonalny);
+
+}
